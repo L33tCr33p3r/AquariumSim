1496d3678b5525b8d61d652568d88d3ea15d24a6
coolanimeguy666 <coolanimeguy666@gmail.com>
2022-09-01 14:48:52 -0600

    added fish class

---
+++ BasicFishClass.py
@@ -0,0 +1,9 @@
+class BasicFishClass:
+    #this class will hold basic data, and how it interacts with other fish
+    def __init__(self ):#default constructor
+        #this constructor isn't meant to be used
+        #just to clearify what variables needed
+        self.type = "string"
+        self.age = 0
+        self.age_rage = 0 #use rng to decide life length (if not terminated by fish with predetor type
+
