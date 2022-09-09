3c7019f425a67aabfe767cb52182a7d23b5a2552
Terminal Tyrant <kubaska05@gmail.com>
2022-09-09 13:59:13 -0600

    MAIN changes

--- AquariumSim.py
+++ AquariumSim.py
@@ -1,37 +1,31 @@
+'''Main File'''
 import pygame
 from BasicFishClass import *
-# This file will be main. 
-# Initialize pygame here 
+
+if __name__ == "__main__":
+    pygame.init()
+    screen = pygame.display.set_mode((1920,1080))#add screen scaling when in window mode
+    #mouse vars
+    MousePos = (0,0)
 
 
-#       Make sure to keep it organized 
-#       
-#       At the top of files make sure to provide a brief, and informative description of it's purpose 
-#
-#globals
-pygame.init()
-screen = pygame.display.set_mode((1920,1080))#add screen scaling when in window mode
-#mouse vars
-MousePos = (0,0)
-
-
-running = True
-while running:
-    #game loop, instantiate some fish in another file.
-    for event in pygame.event.get():#event loop here
+    running = True
+    while running:
+        #game loop, instantiate some fish in another file.
+        for event in pygame.event.get():#event loop here
             if event.type == pygame.QUIT:
-               running = False
-            keys = pygame.key.get_pressed()
+                running = False
+                keys = pygame.key.get_pressed()
             if keys[pygame.K_LCTRL]:
                 running = False
             if event.type == pygame.MOUSEBUTTONDOWN:
                 MousePos = pygame.mouse.get_pos()#gets mouse pos
-               #click 
-            
-    screen.fill((0,0,0))
-    
-    #draw section/IO
-    pygame.display.flip()
+                #click 
+                
+        screen.fill((0,0,0))
+        
+        #draw section/IO
+        pygame.display.flip()
 
-    
-pygame.quit()
+        
+    pygame.quit()
