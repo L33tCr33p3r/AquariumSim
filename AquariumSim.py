ee5f5eca0bd9e689d4062372e6419a56de060492
Terminal Tyrant <kubaska05@gmail.com>
2022-09-13 13:18:46 -0600

    made drawing work

--- AquariumSim.py
+++ AquariumSim.py
@@ -4,26 +4,23 @@ from Fish import *
 
 if __name__ == '__main__':
     pygame.init()
-    screen = pygame.display.set_mode((1920,1080)) #add screen scaling when in window mode
-    #mouse vars
+    screen = pygame.display.set_mode((1920,1080))
     MousePos = (0,0)
 
     Background = pygame.image.load("Background.png")
     
     running = True
     while running:
-        #game loop, instantiate some fish in another file.
-        for event in pygame.event.get():#event loop here
+        screen.blit(Background, (0,0))
+        for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 running = False
             keys = pygame.key.get_pressed()
             if keys[pygame.K_LCTRL]:
                 running = False
             if event.type == pygame.MOUSEBUTTONDOWN:
-                MousePos = pygame.mouse.get_pos()#gets mouse pos
+                MousePos = pygame.mouse.get_pos()
                 pygame.draw.circle(screen,(255,255,255),(MousePos),50)
-        #draw
-        screen.blit(Background, (0,0))
 
         pygame.display.flip()
     pygame.quit()
