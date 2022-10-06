8c09be704f9563e00929d0c22ab23b470411ac94
Terminal Tyrant <kubaska05@gmail.com>
2022-10-06 14:10:06 -0600

    Code formatting

--- AquariumSim.py
+++ AquariumSim.py
@@ -7,41 +7,59 @@ from bubbles import *
 
 if __name__ == "__main__":
     pygame.init()
-    
-    screen = pygame.display.set_mode((0,0), pygame.FULLSCREEN) #add screen scaling when in window mode
-    screenX,screenY = screen.get_size()
-    
-    MousePos = (0,0)
+
+    screen = pygame.display.set_mode(
+        (0, 0), pygame.FULLSCREEN
+    )  # add screen scaling when in window mode
+    screenX, screenY = screen.get_size()
+
+    MousePos = (0, 0)
     groundHeight = 35
     FrameSizeX = 15
     FrameSizeY = 60
-    
+
     fishies = list()
     for i in range(25):
-        fishies.append(Fish(120,random.randint(25,100),random.randint(25,100),random.randint(100, screenX - 100), random.randint(100,screenY - 100), screenX, screenY))
-    bubles = list()
-        
+        fishies.append(
+            Fish(
+                120,
+                random.randint(25, 100),
+                random.randint(25, 100),
+                random.randint(100, screenX - 100),
+                random.randint(100, screenY - 100),
+                screenX,
+                screenY,
+            )
+        )
+    bubbles = list()
+
     seaweedList = []
-    numOfSw = random.randint(10,21)
+    numOfSw = random.randint(10, 21)
     for i in range(numOfSw):
-        mul = (random.randint(3, 7))
-        seaweedList.append(Seaweed((random.randint(10,(screenX-(FrameSizeX*mul)))),(random.randint(screenY - groundHeight, screenY)-(FrameSizeY*mul)),mul))
-    
+        mul = random.randint(3, 7)
+        seaweedList.append(
+            Seaweed(
+                (random.randint(10, (screenX - (FrameSizeX * mul)))),
+                (random.randint(screenY - groundHeight, screenY) - (FrameSizeY * mul)),
+                mul,
+            )
+        )
+
     Background = pygame.image.load("Background.png")
-    Background = pygame.transform.scale(Background,(screenX,screenY))
-    
+    Background = pygame.transform.scale(Background, (screenX, screenY))
+
     interval: float = 10
-    spawnPoint: int = random.randint(100,screenX - 100)
-    spawnPoint2: int = random.randint(100,screenX - 100)
-    
+    spawnPoint: int = random.randint(100, screenX - 100)
+    spawnPoint2: int = random.randint(100, screenX - 100)
+
     clock = pygame.time.Clock()
-    
+
     frameRate = 60
     frameRateNum = 0
     running = True
     while running:
         clock.tick(frameRate)
-        screen.blit(Background, (0,0))
+        screen.blit(Background, (0, 0))
         for event in pygame.event.get():
 
             if event.type == pygame.QUIT:
@@ -54,27 +72,35 @@ if __name__ == "__main__":
                 pygame.draw.circle(screen, (255, 255, 255), (MousePos), 50)
 
         for i in seaweedList:
-            i.draw(screen,frameRateNum)
+            i.draw(screen, frameRateNum)
 
         frameRateNum += 1
         frameRateNum % frameRate
-        
-        if pygame.time.get_ticks() % (1000 * interval) < 30: # declan: this code is really stupid, it should be better, but this is easier to write. Eli: nah
-            spawnPoint = random.randint(100,screenX - 100)
-        if pygame.time.get_ticks() % (1000 * (interval / 15)) < 30: # declan: this code is really stupid, it should be better, but this is easier to write. Eli: nah
-            bubles.append(Bubles(spawnPoint, screenY))
-        if pygame.time.get_ticks() % (1200 * interval) < 29: # declan: this code is really stupid, it should be better, but this is easier to write. Eli: nah
-            spawnPoint2 = random.randint(100,screenX - 100)
-        if pygame.time.get_ticks() % (1000 * (interval / 20)) < 30: # declan: this code is really stupid, it should be better, but this is easier to write. Eli: nah
-            bubles.append(Bubles(spawnPoint2, screenY))
+
+        if (
+            pygame.time.get_ticks() % (1000 * interval) < 30
+        ):  # declan: this code is really stupid, it should be better, but this is easier to write. Eli: nah
+            spawnPoint = random.randint(100, screenX - 100)
+        if (
+            pygame.time.get_ticks() % (1000 * (interval / 15)) < 30
+        ):  # declan: this code is really stupid, it should be better, but this is easier to write. Eli: nah
+            bubbles.append(Bubble(spawnPoint, screenY))
+        if (
+            pygame.time.get_ticks() % (1200 * interval) < 29
+        ):  # declan: this code is really stupid, it should be better, but this is easier to write. Eli: nah
+            spawnPoint2 = random.randint(100, screenX - 100)
+        if (
+            pygame.time.get_ticks() % (1000 * (interval / 20)) < 30
+        ):  # declan: this code is really stupid, it should be better, but this is easier to write. Eli: nah
+            bubbles.append(Bubble(spawnPoint2, screenY))
 
         for f in fishies:
             f.update(screen)
-            
-        for b in bubles:
+
+        for b in bubbles:
             b.update(screen)
             if b.should_delete(screen):
-                bubles.remove(b)
+                bubbles.remove(b)
                 del b
 
         pygame.display.flip()
--- Fish.py
+++ Fish.py
@@ -1,45 +1,59 @@
 import random
 import pygame
+
+
 class Fish:
-    #parent class for all fish, unnecessary if all fish have the same behavior
+    # parent class for all fish, unnecessary if all fish have the same behavior
     def __init__(self, LifeSpanMax, Width, Height, X, Y, screenX, screenY):
-        self.age = 0 #is born, has lived no time.
-        self.age_range = LifeSpanMax #use rng to decide when this fish will die of old age
+        self.age = 0  # is born, has lived no time.
+        self.age_range = (
+            LifeSpanMax  # use rng to decide when this fish will die of old age
+        )
         self.width = Width
         self.height = Height
         self.x = X
         self.y = Y
         self.screenX = screenX
         self.screenY = screenY
-        self.color = (random.randint(0,255),random.randint(0,255),random.randint(0,255))
+        self.color = (
+            random.randint(0, 255),
+            random.randint(0, 255),
+            random.randint(0, 255),
+        )
         self.turn()
-        
+
     def turn(self):
-        self.vx = random.random() + random.randint(-2,1)
-        self.vy = random.random() + random.randint(-2,1)
-        
+        self.vx = random.random() + random.randint(-2, 1)
+        self.vy = random.random() + random.randint(-2, 1)
+
     def update(self, screen):
-        r = random.randint(1,100)
+        r = random.randint(1, 100)
         if r == 1:
             self.turn()
         self.collide()
         self.x += self.vx
         self.y += self.vy
-        pygame.draw.rect(screen, self.color, ((self.x, self.y),(self.width, self.height)))
-        
+        pygame.draw.rect(
+            screen, self.color, ((self.x, self.y), (self.width, self.height))
+        )
+
     def collide(self):
-        #wall collision
+        # wall collision
         if self.x + self.width > self.screenX or self.x < 0:
             self.vx *= -1
         if self.y + self.height > self.screenY or self.y < 0:
             self.vy *= -1
-        
+
+
 class Salmon(Fish):
     def __init__(self, LifeSpanMax, Width, Height, X, Y):
-        super().__init__(LifeSpanMax, Width, Height, X, Y) #this uses parent class constructor PLUS
-        self.color = (255,103,51) #salmony color?                 #<-- this. 
-        
+        super().__init__(
+            LifeSpanMax, Width, Height, X, Y
+        )  # this uses parent class constructor PLUS
+        self.color = (255, 103, 51)  # salmony color?                 #<-- this.
+
+
 class Tuna(Fish):
     def __init__(self, LifeSpanMax, Width, Height, X, Y):
         super().__init__(LifeSpanMax, Width, Height, X, Y)
-        self.color = (255,51,101)
+        self.color = (255, 51, 101)
