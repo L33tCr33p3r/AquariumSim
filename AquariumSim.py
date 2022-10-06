d10b240693bbc7278ddf026f9c11818ff895bbad
Terminal Tyrant <kubaska05@gmail.com>
2022-10-06 12:20:32 -0600

    added black, enjoy yall

--- AquariumSim.py
+++ AquariumSim.py
@@ -1,21 +1,29 @@
-'''Main File'''
+"""Main File"""
 import pygame
 from Fish import *
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     pygame.init()
-    screen = pygame.display.set_mode((1920,1080))
-    MousePos = (0,0)
-    
+    screen = pygame.display.set_mode((1920, 1080))
+    MousePos = (0, 0)
+
     fishies = list()
     for i in range(25):
-        fishies.append(Fish(120,random.randint(25,100),random.randint(25,100),random.randint(100, 1820), random.randint(100,980)))
+        fishies.append(
+            Fish(
+                120,
+                random.randint(25, 100),
+                random.randint(25, 100),
+                random.randint(0, 1820),
+                random.randint(0, 980),
+            )
+        )
 
     Background = pygame.image.load("Background.png")
-    
+
     running = True
     while running:
-        screen.blit(Background, (0,0))
+        screen.blit(Background, (0, 0))
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 running = False
@@ -24,8 +32,8 @@ if __name__ == '__main__':
                 running = False
             if event.type == pygame.MOUSEBUTTONDOWN:
                 MousePos = pygame.mouse.get_pos()
-                pygame.draw.circle(screen,(255,255,255),(MousePos),50)
-                
+                pygame.draw.circle(screen, (255, 255, 255), (MousePos), 50)
+
         for f in fishies:
             f.update(screen)
 
